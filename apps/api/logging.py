37c45c6c4eabece574b6710196413642736a5e4f
tiroq <isshamray@gmail.com>
2026-03-19 16:15:32 +0000

    Remove add_logger_name processor from logging configuration

--- apps/api/logging.py
+++ apps/api/logging.py
@@ -14,7 +14,6 @@ def configure_logging() -> None:
         processors=[
             structlog.contextvars.merge_contextvars,
             structlog.stdlib.add_log_level,
-            structlog.stdlib.add_logger_name,
             structlog.processors.TimeStamper(fmt="iso"),
             structlog.processors.StackInfoRenderer(),
             structlog.processors.format_exc_info,
